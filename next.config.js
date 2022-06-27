dfb3589b679c6b7c0783f45927a555eb98100a68
codeyash02 <yash.codedrill@gmail.com>
2022-06-27 13:09:51 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+const graphql = require('next-plugin-graphql');
+const withPlugins = require('next-compose-plugins');
+ 
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: 'build',
+  webpack: (config, options) => {
+    config.resolve.alias['@components'] = path.resolve(__dirname,'./components');
+    config.resolve.alias['@graphql'] = path.resolve(__dirname,'./graphql-documents');
+    config.resolve.alias['@client'] = path.resolve(__dirname,'./client');
+    config.resolve.alias['@style'] = path.resolve(__dirname,'./style');
+ 
+    return config;
+  },
+};
+ 
+module.exports = withPlugins([
+  // add plugins here..
+  graphql
+], nextConfig);
+
